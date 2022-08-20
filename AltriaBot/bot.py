0ed23aadf533f03ee301e17a861fcb865410b442
joslu <Joseph.Lu@amd.com>
2022-08-21 01:52:38 +0800

    Load src/plugins

--- AltriaBot/bot.py
+++ AltriaBot/bot.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import nonebot
+from nonebot.adapters.onebot.v11 import Adapter as ONEBOT_V11Adapter
 
 # Custom your logger
 # 
@@ -18,6 +19,10 @@ app = nonebot.get_asgi()
 
 driver = nonebot.get_driver()
 
+driver = nonebot.get_driver()
+driver.register_adapter(ONEBOT_V11Adapter)
+
+nonebot.load_plugins("src/plugins")
 
 # Please DO NOT modify this file unless you know what you are doing!
 # As an alternative, you should use command `nb` or modify `pyproject.toml` to load plugins
